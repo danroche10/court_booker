4f11f773413f5bae08de3a0fc0c3b9872c7e6c86
danroche10 <danielroche10@gmail.com>
2022-07-23 13:32:31 +0100

    updated test time

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def job():
   #WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(., 'Pay')]"))).click()
   print("hello3")
 
-schedule.every().day.at("13:32").do(job)
+schedule.every().day.at("13:34").do(job)
 
 while True:
     schedule.run_pending()
