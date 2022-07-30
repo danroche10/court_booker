c826bbea68c3833078c42ae3075b37840091c492
danroche10 <danielroche10@gmail.com>
2022-07-30 15:39:44 +0100

    moved heroku chromeoptions logic into own method

--- app.py
+++ app.py
@@ -11,17 +11,18 @@ import os
 load_dotenv()
 
 def fill_out_booking_details():
-  #chrome_options = webdriver.ChromeOptions()
-  #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-  #chrome_options.add_argument("--headless")
-  #chrome_options.add_argument("--disable-dev-shm-usage")
-  #chrome_options.add_argument("--no-sandbox")
+
+  #add_chrome_options_for_heroku()
+  
+  # browser for production version
   #browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
   booking_date = (datetime.today() + timedelta(days=5)).strftime('%Y-%m-%d')
   url = 'https://bookings.better.org.uk/location/islington-tennis-centre/tennis-court-outdoor/{}/by-time/slot/11:00-12:00'.format(booking_date)
   browser = webdriver.Chrome(executable_path='./chromedriver')
   browser.get(url)
+
+  
   time.sleep(2)
   get_list_of_courts(browser)
   time.sleep(2)
@@ -39,6 +40,14 @@ def fill_out_booking_details():
   #pay_for_booking(browser)
   time.sleep(15)
   print("script complete")
+
+def add_chrome_options_for_heroku():
+  chrome_options = webdriver.ChromeOptions()
+  chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+  chrome_options.add_argument("--headless")
+  chrome_options.add_argument("--disable-dev-shm-usage")
+  chrome_options.add_argument("--no-sandbox")
+
 
 def get_list_of_courts(browser):
   WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class=' css-xz6p7f']"))).click()
@@ -110,7 +119,7 @@ def pay_for_booking(browser):
   WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(., 'Pay')]"))).click()
 
 def schedule_job():
- schedule.every().day.at("15:14").do(book_court)
+ schedule.every().day.at("15:38").do(book_court)
  while True:
   schedule.run_pending()
   time.sleep(1)
@@ -124,8 +133,8 @@ def book_court():
     print(ex)
     sys.exit(0)
 
-book_court()
-#schedule_job()
+#book_court()
+schedule_job()
 
 
 
