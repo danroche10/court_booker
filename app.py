37c09b4d1ab9cb0d76b2b2e20f5f2679050e3814
danroche10 <danielroche10@gmail.com>
2022-08-06 16:23:56 +0100

    refactored and tries to book for next Monday

--- app.py
+++ app.py
@@ -11,12 +11,13 @@ import os
 load_dotenv()
 import os
 
-booking_date = (datetime.today() + timedelta(days=5)).strftime('%Y-%m-%d')
-url = 'https://bookings.better.org.uk/location/islington-tennis-centre/highbury-tennis/{}/by-time/slot/20:00-21:00'.format(booking_date)
+#booking_date = (datetime.today() + timedelta(days=5)).strftime('%Y-%m-%d')
+next_monday = (datetime.today() + timedelta( (1-datetime.today().weekday()) % 7 )).strftime('%Y-%m-%d')
+url = 'https://bookings.better.org.uk/location/islington-tennis-centre/highbury-tennis/{}/by-time/slot/19:00-20:00'.format(next_monday)
 
-def fill_out_booking_details(url):
+def attempt_court_booking(url):
 
-  # browser for production version
+  # browser + chrome_options for production version
   chrome_options = add_chrome_options_for_heroku()
   browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
@@ -24,8 +25,10 @@ def fill_out_booking_details(url):
   #browser = webdriver.Chrome(executable_path='./chromedriver')
   browser.get(url)
 
-  find_court(browser)
-  confirm_payment(browser)
+  if is_court_confirmed(browser) == True:
+    confirm_payment(browser)
+  else:
+    return False
 
 def add_chrome_options_for_heroku():
   chrome_options = webdriver.ChromeOptions()
@@ -37,57 +40,29 @@ def add_chrome_options_for_heroku():
 
 def get_list_of_courts(browser):
   WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class=' css-xz6p7f']"))).click()
-  time.sleep(2)
+  time.sleep(1)
 
 def login(browser):
   WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='username']"))).send_keys(os.environ.get("username"))
   WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='password']"))).send_keys(os.environ.get("password"))
   showmore_link = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, ".//button[contains(@class,'Button__StyledButton-sc-5h7i9w-1 fBHwHD SharedLoginComponent__LoginButton-sc-hdtxi2-5 fQXEJi') and @type='submit']")))
   showmore_link.click()
-  time.sleep(2)
+  time.sleep(1)
 
-def check_for_available_court(browser):
-  for x in range(11):
-    if len(browser.find_elements(By.XPATH, '//div[text()="Highbury Fields Tennis Court ${x}"]')) > 0:
-      browser.find_element(By.XPATH, '//div[text()="Highbury Fields Tennis Court ${x}"]').click()
+def is_court_available(browser):
+  for x in range(1, 11):
+    court_div = '//div[text()="Highbury Fields Tennis Court {}"]'.format(x)
+    if len(browser.find_elements(By.XPATH, court_div)) > 0:
+      browser.find_element(By.XPATH, court_div).click()
       time.sleep(2)
-    # elif len(browser.find_elements(By.XPATH, '//div[text()="Highbury Fields Tennis Court 2"]')) > 0:
-    #   browser.find_element(By.XPATH, '//div[text()="Highbury Fields Tennis Court 2"]').click()
-    #   time.sleep(2)
-    # elif len(browser.find_elements(By.XPATH, '//div[text()="Highbury Fields Tennis Court 3"]')) > 0:
-    #   browser.find_element(By.XPATH, '//div[text()="Highbury Fields Tennis Court 3"]').click()
-    #   time.sleep(2)
-    # elif len(browser.find_elements(By.XPATH, '//div[text()="Highbury Fields Tennis Court 4"]')) > 0:
-    #   browser.find_element(By.XPATH, '//div[text()="Highbury Fields Tennis Court 4"]').click()
-    #   time.sleep(2)
-    # elif len(browser.find_elements(By.XPATH, '//div[text()="Highbury Fields Tennis Court 5"]')) > 0:
-    #   browser.find_element(By.XPATH, '//div[text()="Highbury Fields Tennis Court 5"]').click()
-    #   time.sleep(2)
-    # elif len(browser.find_elements(By.XPATH, '//div[text()="Highbury Fields Tennis Court 6"]')) > 0:
-    #   browser.find_element(By.XPATH, '//div[text()="Highbury Fields Tennis Court 6"]').click()
-    #   time.sleep(2)
-    # elif len(browser.find_elements(By.XPATH, '//div[text()="Highbury Fields Tennis Court 7"]')) > 0:
-    #   browser.find_element(By.XPATH, '//div[text()="Highbury Fields Tennis Court 7"]').click()
-    #   time.sleep(2)
-    # elif len(browser.find_elements(By.XPATH, '//div[text()="Highbury Fields Tennis Court 8"]')) > 0:
-    #   browser.find_element(By.XPATH, '//div[text()="Highbury Fields Tennis Court 8"]').click()
-    #   time.sleep(2)
-    # elif len(browser.find_elements(By.XPATH, '//div[text()="Highbury Fields Tennis Court 9"]')) > 0:
-    #   browser.find_element(By.XPATH, '//div[text()="Highbury Fields Tennis Court 9"]').click()
-    #   time.sleep(2)
-    # elif len(browser.find_elements(By.XPATH, '//div[text()="Highbury Fields Tennis Court 10"]')) > 0:
-    #   browser.find_element(By.XPATH, '//div[text()="Highbury Fields Tennis Court 10"]').click()
-    #   time.sleep(2)
-    # elif len(browser.find_elements(By.XPATH, '//div[text()="Highbury Fields Tennis Court 11"]')) > 0:
-    #   browser.find_element(By.XPATH, '//div[text()="Highbury Fields Tennis Court 11"]').click()
-    #   time.sleep(2)
-    else:
-      print("court unavailable")
+      return True
+  print("court unavailable")
+  return False
 
 def confirm_booking(browser):
   showmore_link2 = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, ".//button[contains(@class,'Button__StyledButton-sc-5h7i9w-1 fBHwHD') and @type='button']")))
   showmore_link2.click()
-  time.sleep(2)
+  time.sleep(1)
 
 def agree_to_terms_and_conditions(browser):
   WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(., 'Continue')]"))).click()
@@ -111,30 +86,33 @@ def fill_out_payment_details(browser):
 def pay_for_booking(browser):
   WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(., 'Pay')]"))).click()
 
-def find_court(browser):
+def is_court_confirmed(browser):
   get_list_of_courts(browser)
-  check_for_available_court(browser)
-  confirm_booking(browser)  
-  login(browser)
-  get_list_of_courts(browser)
-  check_for_available_court(browser)
-  confirm_booking(browser) 
+  if is_court_available(browser) == True:
+    confirm_booking(browser)  
+    login(browser)
+    # above step must be repeated after logging in
+    get_list_of_courts(browser)
+    is_court_available(browser)
+    confirm_booking(browser)
+  else:
+    return False
 
 def confirm_payment(browser):
   fill_out_payment_details(browser)
   #agree_to_terms_and_conditions(browser) // no longer needed
   #pay_for_booking(browser)
-  time.sleep(2)
+  time.sleep(1)
 
 def schedule_job():
- schedule.every().saturday.at("13:53").do(book_court)
+ schedule.every().minute.at(":17").do(book_court)
  while True:
   schedule.run_pending()
   time.sleep(1)
 
 def book_court():
   try:
-    fill_out_booking_details(url)
+    attempt_court_booking(url)
   except Exception as ex:
     # add proper error handling
     print(ex)
